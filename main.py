1477f10b5bca26dc9b4bf097a7e51ab4fbd31b2b
cjones1047 <cjmj1047@gmail.com>
2023-02-22 07:25:40 -0600

    to_dict method added to Cafe that uses dictionary comprehension to create a Python dictionary with column names as keys and row values as values; return value of to_dict method is returned when random_cafe route is hit

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import random
 
 app = Flask(__name__)
 
 ##Connect to Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/CaseyJr/Dropbox/100_days_of_python/cafe_api_flask/cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -23,10 +24,22 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/random")
+def random_cafe():
+    all_books = db.session.query(Cafe).all()
+    print(all_books)
+    random_cafe_choice = random.choice(all_books)
+
+    return jsonify(cafe=random_cafe_choice.to_dict())
     
 
 ## HTTP GET - Read Record
